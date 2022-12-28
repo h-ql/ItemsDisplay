857c0f0b1bad725d40fea5db0523c5b26b032de9
TigerPleb <admirallqh@gmail.com>
2022-12-29 04:57:55 +0700

    Add supported PORT 8080 for GCP deployment

--- src/Program.cs
+++ src/Program.cs
@@ -5,11 +5,6 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 
-// Cloud run:
-// var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-// var url = $"http://0.0.0.0:{port}";
-// builder.WebHost.UseUrls(url);
-
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.ConfigureSerilog();
@@ -24,9 +19,15 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    // Cloud run:
+    var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+    var url = $"http://0.0.0.0:{port}";
+    builder.WebHost.UseUrls(url);
+
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    
 }
 
 // Configure OpenAPI
